490d5eb83252b2b088869e25079f9f9268d53734
Eldalye4 <ahmedeldaly2005@icloud.com>
2024-11-20 18:43:46 +0200

    Update models.py

--- backend/PostDocs/posts/models.py
+++ backend/PostDocs/posts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
-# Create your models here.
+class post(models.Model):
+    
+    titel=models.CharField(max_length=100)
+    content=models.TextField(max_length=100000)
+    
+    data=models.DateField(auto_now_add=True)
+    def __str__(self):
+        return self.titel

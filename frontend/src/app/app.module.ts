6a514c8705f4ebd94e1043e70c0aa30bf2ac03a8
Yaser M. Shabana <yaser.m.shabana@gmail.com>
2024-11-21 17:46:15 +0200

    Feat( landing page ): create home component
    - Transfers the main content from <appComponent> to <home component>.
    - Add the default route and a route to home component.
    - Create resources section in the landing page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,

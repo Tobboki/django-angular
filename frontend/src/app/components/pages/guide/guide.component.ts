2b5e8921840f529ff45f91ee5d63ed8f6cddc1d0
Yaser M. Shabana <yaser.m.shabana@gmail.com>
2024-11-22 20:04:25 +0200

    feat: use <prismService> to highlight code in <guideComponent>

--- frontend/src/app/components/pages/guide/guide.component.ts
+++ frontend/src/app/components/pages/guide/guide.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component, AfterViewInit } from '@angular/core';
+import { PrismService } from '../../../services/prism.service';
 @Component({
   selector: 'app-guide',
   templateUrl: './guide.component.html',
-  styleUrl: './guide.component.css'
+  styleUrl: './guide.component.css',
+  
 })
 export class GuideComponent {
+  constructor( private prismService: PrismService ) {}
 
+  ngAfterViewInit(): void {
+    this.prismService.highlightAll();
+  }
 }

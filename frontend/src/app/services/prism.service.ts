42ce49edafec67815ed9f772fe51b8a34bbc6869
Yaser M. Shabana <yaser.m.shabana@gmail.com>
2024-11-22 19:52:07 +0200

    Feat: create prism service to be later injected into components

---
+++ frontend/src/app/services/prism.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import * as Prism from 'prismjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PrismService {
+  constructor() { }
+  
+    // This method can be used to highlight code on the page after rendering
+    highlightAll(): void {
+      Prism.highlightAll();
+    }
+
+  // This method highlights code in a certain element
+  highlight(codeElement: HTMLElement): void {
+    Prism.highlightElement(codeElement);
+  }
+
+  // This method highlights code under a certain parent
+  highlightAllUnder(container: HTMLElement): void {
+    Prism.highlightAllUnder(container);
+  }
+}
